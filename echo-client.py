e1d66a119ba9d9bf7cfd69e6663439e6059e9846
spiderkiller13 <luckyyu910645@gmail.com>
2019-01-21 17:09:10 +0800

    Test on client

--- echo-client.py
+++ echo-client.py
@@ -1,16 +1,11 @@
 import socket
 
-HOST = '195.0.0.13'# '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '195.0.0.13'  # The server's hostname or IP address
+PORT = 1        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
