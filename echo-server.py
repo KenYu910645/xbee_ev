510b4652f75693b3ffb8c1a17f4d2290f664d325
advancedroboticsaws <advanced.robotics.aws@gmail.com>
2019-01-21 17:08:44 +0800

    Test on server

--- echo-server.py
+++ echo-server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '195.0.0.13'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 1        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
